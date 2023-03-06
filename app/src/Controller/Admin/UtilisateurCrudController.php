22efb532e6c86d8031f3c8ae4fdf4552ac963a3c
Harteos7 <harteos14@gmail.com>
2023-03-06 16:31:34 +0100

    2 commit

---
+++ app/src/Controller/Admin/UtilisateurCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Utilisateur;
+use App\Entity\User;// pour afficher la table User il faut l'appeler
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UtilisateurCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class; // donne la table User à la vue
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
+?>
